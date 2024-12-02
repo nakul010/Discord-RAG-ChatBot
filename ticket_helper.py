e5a990adca31ee340fb108ed8fa778d6f72f86ed
lhy-hoyin <88828097+lhy-hoyin@users.noreply.github.com>
2024-12-02 18:22:42 +0800

    State machine mechanism to help open a ticket

---
+++ ticket_helper.py
@@ -0,0 +1,263 @@
+import discord
+
+
+base_url = "https://stackuphelpcentre.zendesk.com/hc/en-us/requests/new"
+
+
+start_ticket_embed = discord.Embed(
+    title="Submit a request",
+    description="Jumpstart on opening a ticket",
+    url=base_url)
+
+
+class State:
+
+
+    def __init__(self, name):
+        self.name = name
+        self.content = ""
+        self.data: dict = {}
+        self.prev: State = None
+        self.next: State = None
+        self.branch: dict = {}
+
+
+    def insert_next_state(self, state_name):
+        """insert and returns this state object"""
+        new_state = State(state_name)
+        self.next = new_state
+        new_state.prev = self
+        return new_state
+
+
+    def update_content(self, content):
+        """update this state's data, and return this state object"""
+        self.content = content
+        return self
+    
+
+    def update_data(self, key, value):
+        self.data[key] = value
+        return self
+    
+
+    def remove_data(self, key):
+        self.data.popitem(key)
+        return self
+    
+
+    def has_next(self):
+        return self.next is not None or len(self.branch) > 0
+
+
+    def has_prev(self):
+        return self.prev is not None
+
+
+class StateMachine:
+
+    start_state = None
+    current_state = None
+
+    """
+    states = (
+        "start",
+        ("issue_type", (
+            ("general_enquiry", "open_ticket=9094359542041"),
+            ("account_related", "open_ticket=10970588074137"),
+            ("withdrawal_related", "check_processing_days", "open_ticket=11749552676121"),
+
+            ("platform_bug", "suggest_discord", "open_ticket=11733831427737"),
+            ("submission_related", "suggest_discord", "open_ticket=11733869435673")
+        )),
+    )
+    """
+
+
+    issue_type_options = {
+        "General Enquiry": "general_enquiry",
+        "Withdrawal-Related Matters": "withdrawal_related",
+        "Submission-Related Matters": "submission_related",
+        "Account-Related Matters": "account_related",
+        "Platform Bug Issue": "platform_bug"
+    }
+
+
+    def __init__(self):
+        self.start_state = State("start")
+        branch_state = self.start_state.insert_next_state("issue_type").update_content("Choose your issue type:")
+
+        state_1 = State("general_enquiry")
+        state_1.prev = branch_state
+        state_1.insert_next_state("open_ticket").update_data("ticket-id", "9094359542041")
+        branch_state.branch["general_enquiry"] = state_1
+
+        state_2 = State("account_related")
+        state_2.prev = branch_state
+        state_2.insert_next_state("open_ticket").update_data("ticket-id", "10970588074137")
+        branch_state.branch["account_related"] = state_2
+
+        state_3 = State("withdrawal_related")
+        state_3.prev = branch_state
+        state_3\
+            .insert_next_state("check_processing_days")\
+            .insert_next_state("open_ticket").update_data("ticket-id", "11749552676121")
+        branch_state.branch["withdrawal_related"] = state_3
+
+        state_4 = State("platform_bug")
+        state_4.prev = branch_state
+        (
+        state_4
+            .insert_next_state("suggest_discord")
+            .update_content("content content")
+            .insert_next_state("open_ticket")
+            .update_data("ticket-id", "11733831427737")
+        )
+        branch_state.branch["platform_bug"] = state_4
+
+        state_5 = State("submission_related").update_content("### Submission-Related Matters")
+        state_5.prev = branch_state
+        (
+        state_5
+            .insert_next_state("suggest_discord")
+            .update_content(
+                "- Have you checked recent #re-review submission for similar issues reported?\n"
+                "- Have you discussed with other stackies in #re-review submission?"
+            )
+            .insert_next_state("open_ticket")
+            .update_data("ticket-id", "11733869435673")
+        )
+        branch_state.branch["submission_related"] = state_5
+
+        self.current_state = self.start_state
+
+
+    def prev_state(self):
+        """move to previous state"""
+        self.current_state = self.current_state.prev
+
+    def next_state(self, branch = None):
+        """move to next state"""
+
+        if branch:
+            self.current_state = self.current_state.branch[branch]
+        else:
+            self.current_state = self.current_state.next
+
+        if self.current_state.content == "":
+            self.next_state()
+
+
+class TicketHelper(discord.ui.View):
+
+    state_machine: StateMachine = None
+    embed = None
+    content = ""
+
+    def load_state_ui(self):
+        state = self.state_machine.current_state
+
+        self.content = state.content
+        self.embed = start_ticket_embed if state.name == "start" else None
+        self.clear_items()
+
+        if state.name == "issue_type":
+            print('create')
+            self.branch_select = discord.ui.Select(placeholder="Issue Type", options=
+                    list(map(lambda x: discord.SelectOption(label=x), StateMachine.issue_type_options)))
+            self.branch_select.callback = self.branch_next_state
+            self.add_item(self.branch_select)
+            self.next_btn.disabled = True
+        else:
+            self.branch_select = None
+        
+        if state.has_prev():
+            self.add_item(self.back_btn)
+        if state.has_next() and state.name != "issue_type":
+            self.add_item(self.next_btn)
+
+        if state.name == "open_ticket":
+            ticket_form_id = state.data["ticket-id"]
+            self.link_btn.url = f"{base_url}?ticket_form_id={ticket_form_id}" if ticket_form_id else base_url
+            self.add_item(self.link_btn)
+
+        ############################## TESTING ##############################
+        #self.add_item(self.check_btn)
+        ############################## TESTING ##############################
+
+
+    def __init__(self):
+        super().__init__()
+
+        self.state_machine = StateMachine()
+
+        self.back_btn = discord.ui.Button(label="Back", style=discord.ButtonStyle.grey, row=4)
+        self.back_btn.callback = self.go_prev_state
+
+        self.next_btn = discord.ui.Button(label="Proceed",style=discord.ButtonStyle.blurple, row=4)
+        self.next_btn.callback = self.go_next_state
+
+        self.link_btn = discord.ui.Button(label="Open Ticket",style=discord.ButtonStyle.link, row=4, url=base_url)
+
+        ############################## TESTING ##############################
+        # async def check_curr_state(interaction: discord.Interaction):
+        #    await interaction.response.edit_message(content=self.state_machine.current_state.name)
+            
+        # self.check_btn = discord.ui.Button(label="Check",style=discord.ButtonStyle.danger)
+        # self.check_btn.callback = check_curr_state
+
+
+        self.state_machine.next_state()
+        ############################## TESTING ##############################
+
+        self.load_state_ui()
+
+
+    async def go_prev_state(self, interaction: discord.Interaction):
+        self.state_machine.prev_state()
+        self.load_state_ui()
+
+        await interaction.response.edit_message(
+            content=self.content,
+            embed=self.embed,
+            view=self)
+
+
+    async def go_next_state(self, interaction: discord.Interaction):
+        self.state_machine.next_state()
+        self.load_state_ui()
+
+        await interaction.response.edit_message(
+            content=self.content,
+            embed=self.embed,
+            view=self)
+    
+
+    async def branch_next_state(self, interaction: discord.Interaction):
+        selected_issue_type = self.branch_select.values[0]
+        select_branch_state = StateMachine.issue_type_options[selected_issue_type]
+        
+        print(select_branch_state)
+        
+        #"""
+
+        self.state_machine.next_state(select_branch_state)
+        self.load_state_ui()
+
+        await interaction.response.edit_message(
+            content=self.content,
+            embed=self.embed,
+            view=self)
+
+        """#
+        # FIXME: preferbly, selection option does not go next state immedtately,
+        #        but it enables the proceed button instead
+
+        self.next_btn.disabled = False
+        
+        await interaction.response.defer()
+
+        #"""
+
+
+        
